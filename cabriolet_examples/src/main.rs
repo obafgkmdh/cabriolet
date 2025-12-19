b6520191f019475fe9106ae0f028c016fde3ca20
Ryan El Kochta <git@relkochta.com>
2025-12-19 12:32:03 -0500

    Function pointer support for Timely<TIME>
    TODO: arg to Timely creation should really be wrapped in a labeled block macro as well (to force proper unwrap of timely args to the closure)

--- cabriolet_examples/src/main.rs
+++ cabriolet_examples/src/main.rs
@@ -1,14 +1,23 @@
 extern crate some_macros;
-use some_macros::{labeled_block};
+
+use std::time::Instant;
+
+use some_macros::labeled_block;
 
 use secrets_structs::{LabelNonIdem, LabelTimely, Labeled};
 
 fn main() {
     let x = 1;
 
-    let y: Labeled<i32, LabelTimely<1000>> = Labeled::new(5);
+    let before = Instant::now();
+    let mut y: Labeled<i32, LabelTimely<10>> = Labeled::new(Box::new(move || {
+        let now = Instant::now();
+        let elapsed = now - before;
 
-    let z = labeled_block!(LabelNonIdem {
+        elapsed.as_millis() as i32
+    }) as Box<dyn Fn() -> i32>);
+
+    let mut z = labeled_block!(LabelNonIdem {
         std::thread::sleep(std::time::Duration::from_millis(1500));
 
         let sigma = x + unwrap_labeled(y);
@@ -16,5 +25,5 @@ fn main() {
         wrap_labeled(sigma)
     });
 
-    println!("result: {:?}", z);
+    println!("result: {:?}", z.endorse_idempotent());
 }
--- cabriolet_library/secrets_structs/src/lib.rs
+++ cabriolet_library/secrets_structs/src/lib.rs
@@ -1,3 +1,5 @@
+#![feature(generic_const_exprs)]
+
 use std::time::{Duration, Instant};
 
 #[derive(Clone, Debug)]
@@ -11,77 +13,101 @@ pub trait Label {
 }
 
 pub trait AtMostAsIdemAs<T>: Label {}
-impl<T: Label> AtMostAsIdemAs<T> for T {} // reflexive property
+//impl<T: Label> AtMostAsIdemAs<T> for T {} // reflexive property
 
 impl<const TIME: u64> Label for LabelTimely<TIME> {
-    type MetaData<T> = Instant;
+    type MetaData<T> = (Instant, Box<dyn Fn() -> T>);
 }
 impl Label for LabelNonIdem {
     type MetaData<T> = ();
 }
 
-impl<const TIME: u64> AtMostAsIdemAs<LabelTimely<TIME>> for LabelNonIdem {}
-
-trait Contains<T> {
-    fn new(item: T) -> Self;
-    fn unwrap_unchecked(self) -> T;
+impl<L: Label> AtMostAsIdemAs<L> for LabelNonIdem {}
+impl<const T1: u64, const T2: u64> AtMostAsIdemAs<LabelTimely<T2>> for LabelTimely<T1> where
+    [(); T1 as usize - T2 as usize]: Sized
+{
 }
 
-#[derive(Debug)]
+// TODO: ideally, this wouldn't be pub
+pub trait Contains<T> {
+    type CreationArgs;
+
+    fn new(args: Self::CreationArgs) -> Self;
+    unsafe fn unwrap_unchecked(&mut self) -> T;
+}
+
+//#[derive(Debug)]
 pub struct Labeled<T, L>
 where
+    T: Clone,
     L: Label,
 {
-    val: T,
+    val: Option<T>,
     metadata: L::MetaData<T>,
 }
 
-impl<T> Contains<T> for Labeled<T, LabelNonIdem> {
-    fn new(item: T) -> Self {
+impl<T: Clone> Contains<T> for Labeled<T, LabelNonIdem> {
+    type CreationArgs = T;
+
+    fn new(val: Self::CreationArgs) -> Self {
         Self {
-            val: item,
+            val: Some(val),
             metadata: (),
         }
     }
 
-    fn unwrap_unchecked(self) -> T {
+    unsafe fn unwrap_unchecked(&mut self) -> T {
         self.val
+            .clone()
+            .expect("NonIdem should always have a value")
     }
 }
 
-impl<T, const TIME: u64> Contains<T> for Labeled<T, LabelTimely<TIME>> {
-    fn new(item: T) -> Self {
+impl<T: Clone, const TIME: u64> Contains<T> for Labeled<T, LabelTimely<TIME>> {
+    type CreationArgs = Box<dyn Fn() -> T>;
+
+    fn new(create_fn: Self::CreationArgs) -> Self {
         Self {
-            val: item,
-            metadata: Instant::now() + Duration::from_millis(TIME),
+            val: None,
+            metadata: (Instant::now() + Duration::from_millis(TIME), create_fn),
         }
     }
 
-    fn unwrap_unchecked(self) -> T {
+    unsafe fn unwrap_unchecked(&mut self) -> T {
         let now = Instant::now();
-        let expiry = self.metadata;
+        let (expiry, ref create_fn) = self.metadata;
         if now < expiry {
             self.val
+                .clone()
+                .expect("Timely value was non-expired, so should always have a value")
         } else {
-            unimplemented!("tried to unwrap expired value");
+            let val = create_fn();
+            self.val = Some(val.clone());
+
+            val
         }
     }
 }
 
 #[allow(private_bounds)]
-impl<T, L: Label> Labeled<T, L> where Labeled<T, L>: Contains<T> {
-    pub fn new(item: T) -> Self {
-        <Self as Contains<T>>::new(item)
+impl<T, L> Labeled<T, L>
+where
+    T: Clone,
+    L: Label,
+    Labeled<T, L>: Contains<T>,
+{
+    pub fn new(args: <Self as Contains<T>>::CreationArgs) -> Self {
+        <Self as Contains<T>>::new(args)
     }
 
-    pub fn unwrap_checked<Lp>(self) -> T
+    pub fn unwrap_checked<Lp>(&mut self) -> T
     where
         Lp: AtMostAsIdemAs<L>,
     {
-        self.unwrap_unchecked()
+        unsafe { self.unwrap_unchecked() }
     }
 
-    pub fn endorse_idempotent(self) -> T {
-        self.val
+    pub fn endorse_idempotent(&mut self) -> T {
+        self.unwrap_checked::<LabelNonIdem>()
     }
 }
